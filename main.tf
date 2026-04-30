81ef3d01f3edfe867a304ecbfa7fd0973a37e432
Veselin Filipov <veselinfilipov01@gmail.com>
2026-04-30 14:09:41 +0300

    fix: Update VM image URN and region to switzerlandnorth for deployment

--- main.tf
+++ main.tf
@@ -176,10 +176,10 @@ resource "azurerm_linux_virtual_machine" "main" {
   
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
-  }
+}
   
   tags = {
     Environment = "demo"
