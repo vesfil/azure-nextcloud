0adf5195e7861479c5505f4f9b6a5ec5afe7b9c9
Veselin Filipov <veselinfilipov01@gmail.com>
2026-04-30 14:25:19 +0300

    Change region to germanywestcentral

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 variable "location" {
   description = "Azure регион"
   type        = string
-  default     = "francecentral"
+  default     = "germanywestcentral"
 }
 
 variable "resource_group_name" {
